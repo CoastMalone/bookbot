add90dcd4acee03a3167da6df5d764860696b52d
ToastMalone <herbstaustin2013@gmail.com>
2025-01-12 14:17:42 -0600

    update to main.py, character count

--- main.py
+++ main.py
@@ -3,8 +3,35 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} Words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']}")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def get_num_words(text):
     words = text.split()
@@ -20,6 +47,7 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
 
 
 def get_book_text(path):
